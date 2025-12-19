00a987016590036bf38dd6bf689bdce4e5dd262d
SouzaHyago <hyago.souza0103@gmail.com>
2025-12-19 05:00:15 -0300

    correções na responsividade da tela

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,12 +1,11 @@
 import styles from "./SearchBar.module.css"
 import searchIcon from "@/assets/Search.svg"
-import filterIcon from "@/assets/Filter.svg"
 export default function SearchBar(){
   return(
     <div className={styles.container}>
       <div className={styles.searchContainer}>
-        <img src={searchIcon} alt="" />
-        <input type="text" placeholder="Buscar por cliente ou N° de OS..." className={styles.searchInput} />
+        <img src={searchIcon}  alt="" />
+        <input type="text" placeholder="Nome do cliente ou N° de OS" className={styles.searchInput} />
       </div>
       <div className={styles.filtersContainer}>
         <button className={styles.filter}>Todos</button>
